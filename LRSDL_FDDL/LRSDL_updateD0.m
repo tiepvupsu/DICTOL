d04a712246f1781e46ec9b15d245280d67f81fe8
Tiep Vu <vuhuutiep@gmail.com>
2016-05-19 10:48:56 -0400

    update myrng, convert it from a script to a function

--- LRSDL_FDDL/LRSDL_updateD0.m
+++ LRSDL_FDDL/LRSDL_updateD0.m
@@ -1,17 +1,6 @@
 % function D0 = LRSDL_updateD0(Y, D, D0, Y_range, D_range, opts, X, X0, pars)
 function D0 = LRSDL_updateD0(Y, Y_range, D, D_range, D0, X, X0, opts)
-    % D0 = armin_D0 0.5*normF2(L - D0X0) + eta/2 * nuclearnorm(D0)
-%     Ybar = Y - D*X;
-% 
-%     mask = build_mask(D_range, Y_range);
-%     Yhat = Y - D*(X.*mask);
-% 
-%     % Yhat = LRSDL_buildYhat(Y, D, X, D_range, Y_range);
-% 
-%     L = (Ybar + Yhat)/2;
-    % D0 = minRankDict(Ysum*X0', D0, X0*X0', eta);    
+
     L = Y - D*buildMhat(X, D_range, Y_range)/2;
-%     norm(L - L2)
-%     D0 = minRankDict0(L, X0, opts.lambda3/2, D0, pars);
     D0 = min_rank_dict(D0, L*X0', X0*X0', opts.lambda3, opts);
 end
--- utils/myrng.m
+++ utils/myrng.m
@@ -1,13 +1,14 @@
-%% ================== File info ==========================
+function myrng()
 % Author		: Tiep Vu (http://www.personal.psu.edu/thv102/)
 % Time created	: Wed Jan 27 00:23:58 2016
 % Last modified	: Wed Jan 27 00:23:59 2016
-% Description	: make sure that random function does not return the same output
-%% ================== end File info ==========================
-c = clock; %get current time 
+% Description	: make sure that random function does not return 
+%                 the same output
 
-t = mod(floor(c(6))*13, 100) ; % c(6) is 'second'
-for i = 1: t
-	randi(t);
+    c = clock; %get current time 
+
+    t = mod(floor(c(6))*13, 100); % c(6) is 'second'
+    for i = 1: t
+        randi(t);
+    end
 end
-%rand(1)
