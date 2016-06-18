49d8f02496f449ae0c473a2b43f785a08331b625
tiepvupsu <vuhuutiep@gmail.com>
2016-06-18 11:25:26 -0400

    update get_block

--- utils/get_block_col.m
+++ utils/get_block_col.m
@@ -1,18 +1,23 @@
-function Mi = get_block_col(M, i, col_range)
+function Mi = get_block_col(M, C, col_range)
 	%% ================== File info ==========================
 	% Author		: Tiep Vu (http://www.personal.psu.edu/thv102/)
 	% Time created	: 1/27/2016 2:39:32 AM
 	% Last modified	: 1/27/2016 2:39:36 AM
-	% Description	: Get a column block of a big matrix M. 
+	% Description	: Get column blocks of a big matrix M, blocks indexed by C 
 	% 	INPUT
 	%		M        : the big matrix. M = [M1 , M2 , ... , MC]
-	%		i        : block index 
+	%		C        : block indexes 
 	%		col_range: a vector store the last index of each block. col_range(1) = 0.
 	%					i-th block is indexed by col_range(i)+1: col_range(i+1).
 	% 	OUTPUT 
 	%		Mi: output block matrix  
 	%
 	%% ================== end File info ==========================
-	range = col_range(i) + 1: col_range(i+1);
-	Mi = M(:, range);	
+
+	id_sel = [];
+	for i = 1: numel(C)
+		c = C(i);
+		id_sel = [id_sel, col_range(c) + 1: col_range(c+1)];
+	end 
+	Mi = M(:, id_sel, :);
 end
--- utils/get_block_row.m
+++ utils/get_block_row.m
@@ -1,4 +1,4 @@
-function Mi = get_block_row(M, i, row_range)
+function Mi = get_block_row(M, C, row_range)
 	%% ================== File info ==========================
 	% Author		: Tiep Vu (http://www.personal.psu.edu/thv102/)
 	% Time created	: 1/27/2016 2:44:26 AM
@@ -6,13 +6,17 @@ function Mi = get_block_row(M, i, row_range)
 	% Description	: Get a row block of a big matrix X. 
 	% 	INPUT
 	%		M        : the big matrix. M = [M1 ; M2 ; ... ; MC]
-	%		i        : block index 
+	%		C        : block index 
 	%		row_range: a vector store the last index of each block. row_range(1) = 0.
 	%					i-th block is indexed by col_range(i)+1: col_range(i+1).
 	% 	OUTPUT 
 	%		Mi: output block matrix  
 	%
 	%% ================== end File info ==========================
-	range = row_range(i) + 1: row_range(i+1);
-	Mi = M(range, :);	
+	id_sel = [];
+	for i = 1: numel(C)
+		c = C(i);
+		id_sel = [id_sel, row_range(c) + 1: row_range(c+1)];
+	end 
+	Mi = M(id_sel, : , :);
 end
